927a90afa996a3a66c424eebb4f872990889de62
KoenKarolos <61619692+KoenKarolos@users.noreply.github.com>
2021-07-05 12:36:53 +0200

    Expanded GoBoardView, now displays 9x9 foundation of 9x9 Board.

--- app/src/main/java/com/example/goplayapp/GoBoardView.java
+++ app/src/main/java/com/example/goplayapp/GoBoardView.java
@@ -10,12 +10,15 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import static android.content.ContentValues.TAG;
 
 public class GoBoardView extends View  {
 
+    private int counter = 0;
     private Paint paint;
+    int teal = ContextCompat.getColor(getContext(), R.color.teal_700);
 
     public GoBoardView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -26,16 +29,36 @@ public class GoBoardView extends View  {
         paint = new Paint();
         paint.setColor(Color.LTGRAY);
 
-        canvas.drawRect(100f,200f,100 + 70, 200+70, paint);
+        float width = getWidth();
+        float height = getHeight();
+        float init_height = height/5;
+        float cell_9 = width/9;
+
+        for(int column=0;column<9;column++){
+            for(int row=0;row<9;row++){
+                if (column%2 == 1 && row%2 == 0 || column%2 == 0 && row%2 ==1){
+                    paint.setColor(teal);
+                }else{
+                    paint.setColor(Color.LTGRAY);
+                }
+                canvas.drawRect(
+                        cell_9*column,
+                        init_height+cell_9*row,
+                        cell_9*(column+1),
+                        init_height+cell_9*(row+1),paint);
+
+            }
+        }
         super.onDraw(canvas);
     }
-    public int i = 0;
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                Log.d(TAG,"click " + i);
-                i += 1;
+                paint.setColor(Color.BLACK);
+                Log.d(TAG,"click " + counter);
+                counter += 1;
         }
         return true;
     }
