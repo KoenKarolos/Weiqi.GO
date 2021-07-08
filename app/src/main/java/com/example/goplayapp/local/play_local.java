3d8664fffe209a17037031f18ad9d715d4a07637
KoenKarolos <61619692+KoenKarolos@users.noreply.github.com>
2021-07-08 16:15:26 +0200

    Added Buttons in activity_local.xml to test rerendering board into different sizes. Not functional yet.

--- app/src/main/java/com/example/goplayapp/local/play_local.java
+++ app/src/main/java/com/example/goplayapp/local/play_local.java
@@ -1,11 +1,9 @@
 package com.example.goplayapp.local;
 
+
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.goplayapp.R;
-
 
 public class play_local extends AppCompatActivity {
     @Override
